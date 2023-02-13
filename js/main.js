56b23acc4d93ef5d4ec27809af2db468fe3d4165
Mitchel-JF <mitchellcorporations@gmail.com>
2023-02-13 01:30:55 +0100

    commit 1

---
+++ js/main.js
@@ -0,0 +1,89 @@
+$(document).ready(function () {
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     const sidenav = document.querySelectorAll(".sidenav");
+//     const sidenavInstance = M.Sidenav.init(sidenav);
+//     const collapsible = document.querySelectorAll(".collapsible");
+//     const collapsibleInstance = M.Collapsible.init(collapsible);
+// });
+
+
+// const ctx = document.getElementById("chart1").getContext("2d");
+// const ctx2 = document.getElementById("chart2").getContext("2d");
+// const ctx3 = document.getElementById("chart3").getContext("2d");
+
+// const chart1 = new Chart(ctx, {
+//     type: "pie",
+//     data: {
+//         labels: ["America", "Nigeria", "China"],
+//         datasets: [{
+//             label: "MY first Dataset",
+//             data: [300, 500, 100],
+//             backgroundColor: [
+//                 "#ffa726",
+//                 "#f57c00",
+//                 "#e65100"
+//             ],
+//             hoverOffset: 5,
+//         },],
+//     },
+//     options: {
+//         maintainAspectRatio: false,
+//     }
+// });
+
+// const chart2 = new Chart(ctx2, {
+//     type: 'bar',
+//     data: {
+//         labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+//         datasets: [{
+//             label: 'Days of the week',
+//             data: [12, 19, 3, 5, 2, 3],
+//             borderWidth: 1,
+//             hoverBorderWidth: 3,
+//             hoverBorderColor: "#000"
+//         }]
+//     },
+//     options: {
+//         maintainAspectRatio: false,
+//     }
+// });
+
+// const chart3 = new Chart(ctx3, {
+//     type: 'line',
+//     data: {
+//         labels: ['Day1', 'Day2', 'Day3', 'Day4', 'Day5', 'Day6'],
+//         datasets: [{
+//             label: 'Progress',
+//             data: [12, 19, 3, 50, 2, 30],
+//             borderWidth: 1,
+//             hoverBorderWidth: 2,
+//             hoverBorderColor: "#000",
+//             fill: false,
+//         }]
+//     },
+//     options: {
+//         maintainAspectRatio: false,
+//     }
+// });
